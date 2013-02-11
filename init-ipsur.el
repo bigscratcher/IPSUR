b5c218de10252950a86f3f3d25edfb419639b9bd
G. Jay Kerns <gkerns@ysu.edu>
2013-02-10 23:10:43 -0500

    essentially deleted all references to HTML

--- init-ipsur.el
+++ init-ipsur.el
@@ -4,8 +4,7 @@
 (setq load-path (cons "~/.emacs.d/elpa/ess-20130119.1705/lisp" load-path))
 (require 'ess-site)
 (setq ess-ask-for-ess-directory nil)
-
-(require 'org-e-latex)
+(require 'ox-latex)
 ;; (org-babel-lob-ingest "~/org/td-lob.org")
 (setq org-confirm-babel-evaluate nil)
 (org-babel-do-load-languages
@@ -19,15 +18,18 @@
 (setq org-entities-user nil)
 (add-to-list 'org-entities-user '("space" "\\ " nil " " " " " " "–"))
 (setq org-export-latex-packages-alist nil)
-(setq org-e-latex-pdf-process '("texi2dvi --pdf --clean --verbose --batch %f"))
+; (setq ox-latex-pdf-process '("texi2dvi --pdf --clean --verbose --batch %f"))
+;(setq org-latex-pdf-process (quote ("pdflatex -interaction nonstopmode -output-directory %o %f" "bibtex %b" "pdflatex -interaction nonstopmode -output-directory %o %f" "pdflatex -interaction nonstopmode -output-directory %o %f")))
+(setq org-latex-pdf-process (quote ("texi2dvi --pdf --clean --verbose --batch %f" "bibtex %b" "texi2dvi --pdf --clean --verbose --batch %f" "texi2dvi --pdf --clean --verbose --batch %f")))
+
 ;(require 'org-special-blocks)
-(setq org-e-latex-tables-booktabs nil)
-(setq org-e-latex-title-command nil)
-(setq org-export-latex-hyperref-format "\\ref{%s}")
-(setq org-e-latex-remove-logfiles t)
+(setq ox-latex-tables-booktabs nil)
+(setq ox-latex-title-command nil)
+(setq org-latex-hyperref-format "\\ref{%s}")
+(setq ox-latex-remove-logfiles t)
 (setq org-e-latex-toc-command "\\tableofcontents\n\n")
-(setq org-e-latex-classes nil)
-(add-to-list 'org-e-latex-classes
+(setq ox-latex-classes nil)
+(add-to-list 'ox-latex-classes
             '("scrbook"
                "\\documentclass{scrbook}
                [NO-DEFAULT-PACKAGES]"
